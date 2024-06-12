aa90401614a947133c3415eaef926e76aa6edf6b
FerFilho2301 <f234471@dac.unicamp.br>
2024-06-11 22:03:21 -0300

    feat: Bento Grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
+"use client";
+
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div>
         <Hero />
+        <Grid />
       </div>
     </main>
   );
