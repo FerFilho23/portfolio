f5651ddd5326dc58a7d739bd065958b9ae428d72
FerFilho2301 <f234471@dac.unicamp.br>
2024-06-18 17:11:40 -0300

    feat: Testimonials

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Clients from "@/components/Clients";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
@@ -14,6 +15,7 @@ export default function Home() {
         <Hero />
         <Grid />
         <Projects />
+        <Clients />
       </div>
     </main>
   );
