a130c1975eabec8844f02125a95927d94514d1ca
FerFilho2301 <f234471@dac.unicamp.br>
2024-06-18 16:22:57 -0300

    fix: widesrceen bug

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import { navItems } from "@/data";
 export default function Home() {
   return (
     <main className="relative bg-black-100 justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-ful">
+      <div>
         <FloatingNav navItems={navItems}/>
         <Hero />
         <Grid />
